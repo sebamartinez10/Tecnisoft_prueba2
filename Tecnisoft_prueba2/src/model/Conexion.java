e64e0a79f787ce6a293d4249e7271cd248f54646
Sebastian Martinez Roman <33326705+sebamartinez10@users.noreply.github.com>
2018-05-15 13:38:51 -0400

    Add files via upload
    Subida del proyecto

---
+++ Tecnisoft_prueba2/src/model/Conexion.java
@@ -0,0 +1,65 @@
+package model;
+
+// Interfaces != Clases
+import java.sql.Connection;     // Establece la conexión
+import java.sql.ResultSet;
+/*
+Cuando se ejecuta un select, mysql 
+devuelve una tabla. --> ResultSet (Tabla virtual)
+*/
+import java.sql.DriverManager;         // Cargar el Driver de MySQL
+import java.sql.SQLException;
+import java.sql.Statement;      // Declarar o ejecutar Consultas
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class Conexion {
+    private Statement sen;
+    private ResultSet rs;
+    private Connection con; 
+    
+    public Conexion(
+        String server,  // localhost
+        String bd,      // ? (nombre de BD  )
+        String user,    // root
+        String pass     // ?
+    ) throws ClassNotFoundException, SQLException{
+        String protocolo = "jdbc:mysql://";
+        String lineaUser = "user="+user;
+        String lineaPass = "password="+pass;
+       
+        String url = protocolo +
+                server + "/" +
+                bd + "?" +
+                lineaUser + "&" +
+                lineaPass;
+       
+        System.out.println(url);
+        
+        Class.forName("com.mysql.jdbc.Driver"); // JAR?
+        con = DriverManager.getConnection(url);
+    }
+    
+    // insert, delete, update
+    public void ejecutar(String query) throws SQLException{
+        System.out.println(query);
+        sen = con.createStatement();
+        sen.executeUpdate(query);
+        close();
+    }
+   
+    // select
+    public ResultSet ejecutarSelect(String query) throws SQLException{
+        System.out.println(query);
+        sen = con.createStatement();
+        rs = sen.executeQuery(query);
+        return rs;
+    }
+   
+    public void close() throws SQLException{
+        sen.close();
+}
+
+    
+}
