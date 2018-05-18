9ae3a52f94279cd1ef243c22926d7bba5bf52733
Infectus <bastiansantibanez4@gmail.com>
2018-05-17 23:21:43 -0300

    se modifico la base de datos

--- Tecnisoft_prueba2/src/model/Scrip.sql
+++ Tecnisoft_prueba2/src/model/Scrip.sql
@@ -1,52 +1,52 @@
-
 CREATE DATABASE Prueba_2;
  
 USE Prueba_2;
  
 CREATE TABLE tipoUsuario(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    tipo VARCHAR(10)
+    tipo NVARCHAR(10)
 ); -- SELECT * FROM tipoUsuario;
  
 CREATE TABLE tipoVivienda(
     id INT PRIMARY KEY AUTO_INCREMENT,
-    tipo VARCHAR(30)
+    tipo NVARCHAR(30)
 ); -- SELECT * FROM tipoVivienda;
  
---INSERT INTO tipoVivienda VALUES(NULL,'Casa'),(NULL,'Departamento');
- 
---INSERT INTO tipoUsuario VALUES(NULL, 'Administrador'),(NULL, 'Vendedor');
- 
 CREATE TABLE usuario(
-    run VARCHAR(30) PRIMARY KEY,
-    nombre VARCHAR(30)
-    fk_tipoUsu REFERENCES tipoUsuario(id)
+    run NVARCHAR(30) PRIMARY KEY,
+    nombre NVARCHAR(30),
+    fk_tipoUsu INT,
+    FOREIGN KEY(fk_tipoUsu) REFERENCES tipoUsuario(id)
 ); -- SELECT * FROM usuario;
  
-INSERT INTO usuario VALUES(NULL,'11-1','admin',1);
+
  
 CREATE TABLE vivienda(
-    num_rol VARCHAR(30) PRIMARY KEY,
-    direccion VARCHAR(50),
+    num_rol NVARCHAR(30) PRIMARY KEY,
+    direccion NVARCHAR(50),
     cantPiezas INT,
     cantBaNos INT,
-    fk_tipoVivienda INT  REFERENCES tipoVivienda(id),
+    fk_tipoVivienda INT,
+    FOREIGN KEY(fk_tipoVivienda) REFERENCES tipoVivienda(id),
     precio INT,
     nueva BOOLEAN
 ); -- SELECT * FROM vivienda;
  
  
 CREATE TABLE cliente(
-    run VARCHAR(30) PRIMARY KEY,
-    nombre VARCHAR(50),
+    run NVARCHAR(30) PRIMARY KEY,
+    nombre NVARCHAR(50),
     sueldo INT
 ); -- SELECT * FROM cliente;
  
 CREATE TABLE venta(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    fk_vivienda INT REFERENCES vivienda(id),
-    fk_cliente INT REFERENCES cliente(id),
-    fk_usuario INT REFERENCES usuario(run),
+    fk_vivienda NVARCHAR(30),
+    fk_cliente NVARCHAR(30),
+    fk_usuario NVARCHAR(30),
+    FOREIGN KEY(fk_vivienda)REFERENCES vivienda(num_rol),
+    FOREIGN KEY(fk_cliente) REFERENCES cliente(run),
+    FOREIGN KEY(fk_usuario) REFERENCES usuario(run),
     fecha DATE
    
 ); -- SELECT * FROM venta;
@@ -55,5 +55,4 @@ CREATE TABLE restauracion(
     id INT AUTO_INCREMENT PRIMARY KEY,
     fecha DATE,
     hora TIME
-   
-); -- SELECT * FROM restauracion;
+);
